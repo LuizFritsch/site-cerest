910a99a1259d23be1dfcc1ed91388440217ddbdd
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2020-01-17 11:15:57 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 				<h1 class="text-justify">Inicio</h1>
 				<p class="text-justify">O Centro Regional de Referência em Saúde do Trabalhador da Região Oeste do Rio Grande do Sul, é um serviço do Sistema Único de Saúde (SUS), com sede em Alegrete. Possui abrangência regional atendendo os municípios da 10ª Coordenadoria Regional de Saúde. A Rede Nacional de Atenção Integral à Saúde do Trabalhador (RENAST), regulamentada pela PORTARIA DE CONSOLIDAÇÃO do Ministério da Saúde nº 3 de 28 de setembro de 2017, tem o objetivo de disseminar ações de Saúde do Trabalhador articuladas às demais redes do SUS.</p>
 				<p class="text-justify">Dá suporte técnico para a atenção integral em Saúde do Trabalhador, com ações de Prevenção, Promoção, Diagnóstico, Tratamento, Reabilitação e Vigilância em Saúde dos Trabalhadores e Trabalhadoras Urbanos e Rurais.</p>
-				<p>Conheça mais sobre o CEREST OESTE, <a href="o-cerest.php">clique aqui.</a></p>
+				<p>Conheça mais sobre o CEREST OESTE, <a href="o_cerest.php">clique aqui.</a></p>
 				<div class="atendimento text-center">
 					<img alt="pessoa sendo atendida no cerest" src="./img/frenteCerest.jpg" class="zoom responsive img-fluid">
 					<p class="text-center descricaoImagem"><strong>Fachada do CEREST OESTE.</strong></p>
---
+++ relatorio_nucleos.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Relatório Semestral dos Núcleos em ST</title>
+	</head>
+	<body>
+		<?php include 'header.html'; ?>
+		<main>
+			<div class="content text-break">
+				<h1 class="text-justify">Relatório Semestral dos Núcleos em ST</h1>
+				<div>
+					<form>
+						<div class="form-row">
+							<div class="form-group col-md-6">
+								<label for="inputSecretario">Secretário de Saúde</label>
+								<input type="text" class="form-control" id="inputSecretario" placeholder="Digite o nome do Secretário de Saúde...">
+							</div>
+							<div class="form-group col-md-6">
+								<label for="inputResponsavel">Profissional Responsável</label>
+								<input type="text" class="form-control" id="inputResponsavel" placeholder="Digite o nome do Profissional Responsável...">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputCNES">CNES</label>
+							<input type="text" class="form-control" id="inputCNES" placeholder="Digite o Nº CNES do Núcleos">
+						</div>
+						<hr>
+						<div class="dropdown">
+							<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownAtencaoBasica" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Dropdown button
+							</button>
+							<div class="dropdown-menu" aria-labelledby="dropdownAtencaoBasica">
+								<a class="dropdown-item" href="#">Action</a>
+								<a class="dropdown-item" href="#">Another action</a>
+								<a class="dropdown-item" href="#">Something else here</a>
+							</div>
+						</div>
+
+						<div class="form-row">
+							<div class="form-group col-md-6">
+								<label for="inputCity">Cidade</label>
+								<input type="text" class="form-control" id="inputCity">
+							</div>
+							<div class="form-group col-md-4">
+								<label for="inputEstado">Estado</label>
+								<select id="inputEstado" class="form-control">
+									<option selected>Escolher...</option>
+									<option>...</option>
+								</select>
+							</div>
+							<div class="form-group col-md-2">
+								<label for="inputCEP">CEP</label>
+								<input type="text" class="form-control" id="inputCEP">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="form-check">
+								<input class="form-check-input" type="checkbox" id="gridCheck">
+								<label class="form-check-label" for="gridCheck">
+									Clique em mim
+								</label>
+							</div>
+						</div>
+						<button type="submit" class="btn btn-primary">Entrar</button>
+					</form>
+				</div>
+			</div>
+			<br>
+		</main>
+		<?php include 'footer.html'; ?>
+	</body>
+</html>
