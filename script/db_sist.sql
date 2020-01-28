7254e78061e2757f48781fecee89fbaf905480a2
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2020-01-28 11:32:06 -0300

    Add files via upload

--- script/db_sist.sql
+++ script/db_sist.sql
@@ -2,10 +2,10 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 24-Jan-2020 às 14:37
--- Versão do servidor: 10.1.36-MariaDB
--- versão do PHP: 7.2.10
+-- Host: localhost:3306
+-- Generation Time: 28-Jan-2020 às 08:46
+-- Versão do servidor: 5.6.41-84.1
+-- versão do PHP: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,8 +19,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_sist`
+-- Database: `vinilpub_guilherme_cerest`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcoes`
+--
+
+CREATE TABLE `funcoes` (
+  `ID_FUNCAO` int(11) NOT NULL,
+  `FUNCAO` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `funcoes`
+--
+
+INSERT INTO `funcoes` (`ID_FUNCAO`, `FUNCAO`) VALUES
+(1, 'ADMIN'),
+(2, 'NORMAL');
 
 -- --------------------------------------------------------
 
@@ -41,9 +60,37 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`ID_USER`, `NOME`, `SENHA`) VALUES
 (1, 'gnome', 'batata123');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID_USUARIO` int(11) NOT NULL,
+  `NOME` varchar(40) CHARACTER SET utf8 NOT NULL,
+  `SENHA` varchar(40) CHARACTER SET utf8 NOT NULL,
+  `FK_ID_FUNCAO` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`ID_USUARIO`, `NOME`, `SENHA`, `FK_ID_FUNCAO`) VALUES
+(1, 'gnome', 'batata123', 1),
+(2, 'luiz', 'fritsch', 2);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `funcoes`
+--
+ALTER TABLE `funcoes`
+  ADD PRIMARY KEY (`ID_FUNCAO`),
+  ADD KEY `ID_FUNCAO` (`ID_FUNCAO`);
 
 --
 -- Indexes for table `usuario`
@@ -52,14 +99,43 @@ ALTER TABLE `usuario`
   ADD PRIMARY KEY (`ID_USER`);
 
 --
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID_USUARIO`),
+  ADD KEY `FK_ID_FUNCAO` (`FK_ID_FUNCAO`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `funcoes`
+--
+ALTER TABLE `funcoes`
+  MODIFY `ID_FUNCAO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `ID_USER` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID_USUARIO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`FK_ID_FUNCAO`) REFERENCES `funcoes` (`ID_FUNCAO`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
