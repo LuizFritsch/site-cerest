d399e7d5df02a2b0b9effe1f944d95eb43cf21cf
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2020-02-07 11:08:37 -0300

    Add files via upload
    -Criação do modal para seleção de perfil de usuário

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php include 'HEADER.php'; ?>
 <?php
 	if(isset($_SESSION['login']) && isset($_SESSION['senha'])){
-		echo "<script>window.location.replace('https://guilherme.cerestoeste.com.br/relatorio_nucleos.php#msg');</script>";
-	}
+echo "<script>window.location.replace('https://guilherme.cerestoeste.com.br/relatorio_nucleos.php#msg');</script>";
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -39,9 +39,64 @@
 							<div class="col-sm">
 								<h4 id="t" class="text-justify">Não é cadastrado ainda?</h4>
 								<p class="text-justify">Caso você não esteja cadastrado no nosso sistema ainda, solicite seu usuário e senha.</p>
-								<form method="POST" action="solicitar_usuario.php">
-									<button class="btn btn-success">Solicitar Acesso</button>
-								</form>
+								
+								<button class="btn btn-success" data-toggle="modal" data-target="#ExemploModalCentralizado">Solicitar Acesso</button>
+								<!-- Modal -->
+								<div class="modal fade bd-example-modal-xl" id="ExemploModalCentralizado" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+									<div class="modal-dialog modal-dialog-centered" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h5 class="modal-title" id="TituloModalCentralizado">Solicitar Acesso</h5>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+												<span aria-hidden="true">&times;</span>
+												</button>
+											</div>
+
+
+											<div class="modal-body modal-dialog modal-xl">
+												<div class="modal-body modal-dialog modal-xl">
+													
+
+													<div class="row text-center">
+													
+														<div class="col-md-6">
+															<div class="card m-3 border shadow-sm">
+																<div class="card-body">
+																	<h5 class="card-title">Sou representante de Núcleo</h5>
+																	<hr>
+																	<p class="card-text text-center">Esta opção é para pessoas que fazem parte dos núcleos em saúde do trabalhador.</p>
+																	<p class="card-text text-center">Será solicitado informações relacionadas a você, ao secretário de saúde e do presidente do conselho municipal de saúde do seu municipio.</p>
+																	<p class="card-text text-center"><strong>*Caso opte por esta opção, sua solicitação será avaliada antes de liberarmos seu acesso.</strong></p>
+																	<button class="btn btn-success" data-toggle="modal" data-target="#ExemploModalCentralizado">Solicitar</button>
+																</div>
+															</div>
+														</div>
+														<div class="col-md-6">
+															<div class="card m-3 border shadow-sm">
+																<div class="card-body">
+																	<h5 class="card-title">Sou um usuário Comum</h5>
+																	<hr>
+																	<p class="card-text text-center">Esta opção é para quem apenas deseja se cadastrar para receber emails sobre eventos ou realizar comentários nas páginas de noticia.</p>
+																	<button class="btn btn-success" data-toggle="modal" data-target="#ExemploModalCentralizado">Solicitar</button>
+																</div>
+															</div>
+														</div>
+													
+													</div>
+
+
+												</div>
+											</div>
+
+
+
+											<div class="modal-footer">
+												<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+											</div>
+										</div>
+									</div>
+								</div>
+								
 							</div>
 						</div>
 					</div>
