705750fe78f3619fe70d9b66b5b325e48fc9e538
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2020-02-10 15:54:41 -0300

    Add files via upload
    Correção na admin conselho gestor:
    -Agora ele pega dinamicamente todas funções do conselho gestor e todos os nomes daquelas funções(Caso seja necessário add mais funções, n sera necessário alterar qualquer codigo)

--- admin/admin_conselho_gestor.php
+++ admin/admin_conselho_gestor.php
@@ -34,41 +34,45 @@
 									<thead>
 										<tr>
 											<?php
+												//Seleciona todas as funcoes do conselho gestor que estao do banco
+												$arrayFuncoes = array();
 												$sql="SELECT * FROM funcoes_conselho";
 												$result=mysqli_query($con,$sql);
 												if(!$result ) {
 													die('Could not get data: ' . mysql_error());
 												}
+												//Envia o html contendo o nome das funcoes para um array
 												while($row = mysqli_fetch_array($result)) {
-													echo "<th scope='col'>{$row['NOME_FUNCAO']}</th> ";
+													array_push($arrayFuncoes,"<h6>{$row['NOME_FUNCAO']}:</h6>");
 												}
+
+												//Seleciona todos os nomes dos membros do conselho gestor que estao do banco
+												$arrayNomes = array();
+												$sqll="SELECT * FROM conselho_gestor";
+												$resultt=mysqli_query($con,$sqll);
+												if(!$resultt) {
+													die('Could not get data: ' . mysql_error());
+												}
+												//Envia o html contendo o nome dos membros do conselho gestor para um array
+												while($row = mysqli_fetch_array($resultt)) {
+													//echo "<td contenteditable='true' name='funcao{$row['FK_ID_FUNCAO']}'>{$row['NOME']}</td>";
+												array_push($arrayNomes,"<input type='text' class='form-control' name='funcao{$row['FK_ID_FUNCAO']}' value='{$row['NOME']}'></input>");
+												}
+												$size=count($arrayFuncoes);												
+												echo "$arrayFuncoes[$i]";
+												//Printo na tela todos registros de:
+												//Nome da funcao
+												//Input editavel com o nome do atual mebro do conselho
+												for ($i=0; $i<$size; $i++) {
+													echo "$arrayFuncoes[$i]";
+													echo "$arrayNomes[$i]";
+												}
+
 											?>
 										</tr>
 									</thead>
-									<tbody>
-										<tr>
-											<?php
-												$sql="SELECT * FROM conselho_gestor";
-												$result=mysqli_query($con,$sql);
-												if(!$result ) {
-													die('Could not get data: ' . mysql_error());
-												}
-												while($row = mysqli_fetch_array($result)) {
-													//echo "<td contenteditable='true' name='funcao{$row['FK_ID_FUNCAO']}'>{$row['NOME']}</td>";
-													echo "<td><input type='text' class='form-control' name='funcao{$row['FK_ID_FUNCAO']}' value='{$row['NOME']}'></input></td>";
-												}
-											?>
-										</tr>
-									</tbody>
 								</table>
-								<!--<script type="text/javascript">
-									for (var i=1;i<5; i++) {
-										document.getElementById("funcao".concat(''+i)).addEventListener("input", function() {
-											console.log("input event fired");
-											}, false);
-									}
-									
-								</script>-->
+								
 								<div align="right" style="align-self: right;align-items: right;">
 									<button type="submit" class="btn btn-success">Salvar</button>
 								</div>
@@ -76,28 +80,30 @@
 						</div>
 					</div>
 					<?php
-						if( $_SERVER['REQUEST_METHOD'] == 'POST'){
-							$sql="";
+						if($_SERVER['REQUEST_METHOD'] == 'POST'){
+							$sqlUpdate="";
 							for ($i=1; $i < 5; $i++) {
 								$nome=$_POST['funcao'.$i.''];
-								$sql.="UPDATE conselho_gestor SET NOME='$nome' WHERE FK_ID_FUNCAO= $i;";
+								$sqlUpdate.="UPDATE conselho_gestor SET NOME='$nome' WHERE FK_ID_FUNCAO= $i;";
 							}
-							if(mysqli_multi_query($con,$sql)){
-					echo "<script>Swal.fire(
-					'Sucesso!',
-					'Os membros do conselho foram atualizados com sucesso!',
-					'success'
-					)</script>";
-					echo "<script>location.reload()</script>";
-					}else{
-					echo "<script>Swal.fire({
-					icon: 'Erro',
-					title: 'Oops...',
-					text: 'Não foi possivel alterar os membros do conselho, tente novamente!',
-					})</script>";
-					echo "<script>location.reload()</script>";
-					}
-					}
+							if(mysqli_multi_query($con,$sqlUpdate)){
+								echo "<script>Swal.fire(
+								'Sucesso!',
+								'Os membros do conselho foram atualizados com sucesso!',
+								'success'
+								).then(function() {
+							    	window.location = 'https://guilherme.cerestoeste.com.br/admin/admin_conselho_gestor.php';
+								});</script>";
+							}else{
+								echo "<script>Swal.fire({
+								icon: 'Erro',
+								title: 'Oops...',
+								text: 'Não foi possivel alterar os membros do conselho, tente novamente!',
+								}).then(function() {
+							    	window.location = 'https://guilherme.cerestoeste.com.br/admin/admin_conselho_gestor.php';
+								});</script>";
+							}
+						}
 					?>
 					
 				</form>
