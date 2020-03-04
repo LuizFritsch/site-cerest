4303187848391434e85478c77d9305abd35d1957
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2020-03-04 10:39:04 -0300

    modificacoes bd
    Criação da parte dos núcleos no bd:
        -núcleos tem contatos com tipos (secretario de saude, responsavel pelo nucleo, presidente conselho saude) e tambem usuarios

--- database/db_sist.sql
+++ database/db_sist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: 28-Jan-2020 às 08:46
+-- Generation Time: 04-Mar-2020 às 10:32
 -- Versão do servidor: 5.6.41-84.1
 -- versão do PHP: 7.2.7
 
@@ -21,6 +21,42 @@ SET time_zone = "+00:00";
 --
 -- Database: `vinilpub_guilherme_cerest`
 --
+CREATE DATABASE IF NOT EXISTS `vinilpub_guilherme_cerest` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `vinilpub_guilherme_cerest`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `conselho_gestor`
+--
+
+CREATE TABLE IF NOT EXISTS `conselho_gestor` (
+  `ID_MEMBRO` int(4) NOT NULL AUTO_INCREMENT,
+  `NOME` varchar(50) NOT NULL,
+  `FK_ID_FUNCAO` int(11) NOT NULL,
+  PRIMARY KEY (`ID_MEMBRO`),
+  UNIQUE KEY `FK_ID_FUNCAO_2` (`FK_ID_FUNCAO`),
+  KEY `FK_ID_FUNCAO` (`FK_ID_FUNCAO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `contato`
+--
+
+CREATE TABLE IF NOT EXISTS `contato` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOME` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `TELEFONE` varchar(14) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `EMAIL` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ENDERECO` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `FK_ID_NUCLEO` int(11) NOT NULL,
+  `FK_ID_TIPO` int(11) NOT NULL,
+  PRIMARY KEY (`ID`,`FK_ID_NUCLEO`,`FK_ID_TIPO`),
+  KEY `fk_contato_nucleo_idx` (`FK_ID_NUCLEO`),
+  KEY `fk_contato_tipo1_idx` (`FK_ID_TIPO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -28,37 +64,83 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `funcoes`
 --
 
-CREATE TABLE `funcoes` (
-  `ID_FUNCAO` int(11) NOT NULL,
-  `FUNCAO` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+CREATE TABLE IF NOT EXISTS `funcoes` (
+  `ID_FUNCAO` int(11) NOT NULL AUTO_INCREMENT,
+  `FUNCAO` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_FUNCAO`),
+  KEY `ID_FUNCAO` (`ID_FUNCAO`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Extraindo dados da tabela `funcoes`
---
-
-INSERT INTO `funcoes` (`ID_FUNCAO`, `FUNCAO`) VALUES
-(1, 'ADMIN'),
-(2, 'NORMAL');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Estrutura da tabela `funcoes_conselho`
 --
 
-CREATE TABLE `usuario` (
-  `ID_USER` int(4) NOT NULL,
-  `NOME` varchar(50) NOT NULL,
-  `SENHA` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `funcoes_conselho` (
+  `ID_FUNCAO_CONSELHO` int(11) NOT NULL AUTO_INCREMENT,
+  `NOME_FUNCAO` varchar(70) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_FUNCAO_CONSELHO`),
+  KEY `ID_FUNCAO_CONSELHO` (`ID_FUNCAO_CONSELHO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
--- Extraindo dados da tabela `usuario`
+-- Estrutura da tabela `nucleo`
 --
 
-INSERT INTO `usuario` (`ID_USER`, `NOME`, `SENHA`) VALUES
-(1, 'gnome', 'batata123');
+CREATE TABLE IF NOT EXISTS `nucleo` (
+  `ID` int(11) NOT NULL,
+  `NOME` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `TELEFONE` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `EMAIL` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `LOGO` blob,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID` (`ID`),
+  KEY `ID_2` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `publicacoes`
+--
+
+CREATE TABLE IF NOT EXISTS `publicacoes` (
+  `ID_PUBLICACAO` int(11) NOT NULL AUTO_INCREMENT,
+  `NOME` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `URL` varchar(437) COLLATE utf8_unicode_ci NOT NULL,
+  `DESCRICAO` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `FK_TIPO_PUBLICACAO` int(11) NOT NULL,
+  PRIMARY KEY (`ID_PUBLICACAO`),
+  KEY `FK_TIPO_PUBLICACAO` (`FK_TIPO_PUBLICACAO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipo`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TIPO` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipo_publicacoes`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo_publicacoes` (
+  `ID_TIPO_PUBLICACAO` int(11) NOT NULL AUTO_INCREMENT,
+  `TIPO_PUBLICACAO` varchar(400) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID_TIPO_PUBLICACAO`),
+  KEY `ID_TIPO_PUBLICACAO` (`ID_TIPO_PUBLICACAO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -66,76 +148,65 @@ INSERT INTO `usuario` (`ID_USER`, `NOME`, `SENHA`) VALUES
 -- Estrutura da tabela `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `ID_USUARIO` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `ID_USUARIO` int(11) NOT NULL AUTO_INCREMENT,
   `NOME` varchar(40) CHARACTER SET utf8 NOT NULL,
   `SENHA` varchar(40) CHARACTER SET utf8 NOT NULL,
-  `FK_ID_FUNCAO` int(11) NOT NULL
+  `FK_ID_FUNCAO` int(11) NOT NULL,
+  PRIMARY KEY (`ID_USUARIO`),
+  KEY `FK_ID_FUNCAO` (`FK_ID_FUNCAO`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Extraindo dados da tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`ID_USUARIO`, `NOME`, `SENHA`, `FK_ID_FUNCAO`) VALUES
-(1, 'gnome', 'batata123', 1),
-(2, 'luiz', 'fritsch', 2);
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Estrutura da tabela `usuario_nucleo`
 --
 
---
--- Indexes for table `funcoes`
---
-ALTER TABLE `funcoes`
-  ADD PRIMARY KEY (`ID_FUNCAO`),
-  ADD KEY `ID_FUNCAO` (`ID_FUNCAO`);
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`ID_USER`);
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`ID_USUARIO`),
-  ADD KEY `FK_ID_FUNCAO` (`FK_ID_FUNCAO`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `funcoes`
---
-ALTER TABLE `funcoes`
-  MODIFY `ID_FUNCAO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `ID_USER` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `ID_USUARIO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+CREATE TABLE IF NOT EXISTS `usuario_nucleo` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `USUARIO` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `SENHA` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `FK_ID_NUCLEO` int(11) NOT NULL,
+  PRIMARY KEY (`ID`,`FK_ID_NUCLEO`),
+  UNIQUE KEY `USUARIO_UNIQUE` (`USUARIO`),
+  KEY `fk_usuario_nucleo_nucleo1_idx` (`FK_ID_NUCLEO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Limitadores para a tabela `conselho_gestor`
+--
+ALTER TABLE `conselho_gestor`
+  ADD CONSTRAINT `conselho_gestor_ibfk_1` FOREIGN KEY (`FK_ID_FUNCAO`) REFERENCES `funcoes_conselho` (`ID_FUNCAO_CONSELHO`);
+
+--
+-- Limitadores para a tabela `contato`
+--
+ALTER TABLE `contato`
+  ADD CONSTRAINT `fk_contato_nucleo` FOREIGN KEY (`FK_ID_NUCLEO`) REFERENCES `nucleo` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_contato_tipo1` FOREIGN KEY (`FK_ID_TIPO`) REFERENCES `tipo` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `publicacoes`
+--
+ALTER TABLE `publicacoes`
+  ADD CONSTRAINT `publicacoes_ibfk_1` FOREIGN KEY (`FK_TIPO_PUBLICACAO`) REFERENCES `tipo_publicacoes` (`ID_TIPO_PUBLICACAO`);
+
+--
 -- Limitadores para a tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`FK_ID_FUNCAO`) REFERENCES `funcoes` (`ID_FUNCAO`);
+
+--
+-- Limitadores para a tabela `usuario_nucleo`
+--
+ALTER TABLE `usuario_nucleo`
+  ADD CONSTRAINT `fk_usuario_nucleo_nucleo1` FOREIGN KEY (`FK_ID_NUCLEO`) REFERENCES `nucleo` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
