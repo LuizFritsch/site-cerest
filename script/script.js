36cf69413e49923b0ae801c9bb70d057f85088dd
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2020-03-10 10:21:07 -0300

    metodo para "previnir" sql injection
    -criação de um método que não permite caracteres especiais em inputs

--- script/script.js
+++ script/script.js
@@ -81,3 +81,14 @@ function nucleoAncora(p1, p2) {
 }
 
 
+$(document).ready(function(){
+  $("input").keypress(function(event){
+    var request=$(this).val() + String.fromCharCode(event.which);
+    var lastChar = request.substr(request.length-1);
+    var format = /[!#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    if(format.test(lastChar)){
+      //se tem caracter especial, retira ele
+      event.preventDefault();
+    }
+  });
+});
