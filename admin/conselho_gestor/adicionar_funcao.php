e8cd1a310b511c6076d85180168b6a919fb89691
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2020-03-16 09:28:40 -0300

    modificacoes conselho gestor
    -Exclui o add membro, pois o add eh feito no gerenciar ja, no próprio input
    -primeira versao do form pra add funcao no conselho

---
+++ admin/conselho_gestor/adicionar_funcao.php
@@ -0,0 +1,39 @@
+<?php include '../../HEADER.php'; ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Adicionar Função</title>
+	</head>
+	<body>
+		<?php
+		if(($_SESSION['id']!=1) || (!isset($_SESSION['login'])) && (!isset($_SESSION['senha']))){
+			echo "<script>alert('Você não está logado ou não tem o nível de acesso necessário!')</script>";
+			echo "<script>window.location.replace('https://guilherme.cerestoeste.com.br/login.php');</script>";
+		}else{
+			$logado = $_SESSION['login'];
+			$func = $_SESSION['func'];
+			$senh = $_SESSION['senha'];
+			$ide = $_SESSION['id'];
+		}
+		include '../database/db_connection.php';
+		$con=OpenCon();
+		$logado = $_SESSION['login'];
+		$func = $_SESSION['func'];
+		$senh = $_SESSION['senha'];
+		$ide = $_SESSION['id'];
+		?>
+		<main>
+			<div class="content text-break">
+				<h1 id="t" class="text-justify">Adicionar Função para o conselho Gestor</h1>
+				<form>
+					<div class="form-group">
+						<h6>Digite a Função:</h6>
+						<input type="text" class="form-control" name="nomeFuncao" placeholder="Digite o nome da função que será adicionada ao conselho gestor...">
+					</div>
+				</form>
+				<button type="submit" id="save" name="save" class="btn btn-success save">Adicionar Função</button>
+			</div>
+		</main>
+		<?php include '../footer.html'; ?>
+	</body>
+</html>
