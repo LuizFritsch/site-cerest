30f5cd014f2ec3526b3f43064b2b06140a4964d1
LuizFritsch <fritsch.guilherm3@gmail.com>
2020-04-01 10:58:45 -0300

    edit login
    -procuro nas duas tabelas se o usuario existe

--- database/log_in.php
+++ database/log_in.php
@@ -9,7 +9,7 @@ $senha=$_POST['inputSenha'];
 $con = OpenCon();
 // A variavel $result pega as varias $login e $senha, faz uma
 //pesquisa na tabela de usuarios
-//$result=mysqli_query($con,"SELECT * FROM usuarios WHERE NOME = '$login' AND SENHA= '$senha'");
+$result=mysqli_query($con,"SELECT * FROM usuarios WHERE NOME = '$login' AND SENHA= '$senha'");
 /* Logo abaixo temos um bloco com if e else, verificando se a variável $result foi
 bem sucedida, ou seja se ela estiver encontrado algum registro idêntico o seu valor
 será igual a 1, se não, se não tiver registros seu valor será 0. Dependendo do
@@ -17,7 +17,7 @@ resultado ele redirecionará para a página site.php ou retornara  para a págin
 do formulário inicial para que se possa tentar novamente realizar o login */
 $senhaHash=md5($senha);
 $results=mysqli_query($con,"SELECT * FROM usuario_comum WHERE USUARIO = '$login' AND SENHA= '$senhaHash'");
-if(mysqli_num_rows($results)>0){
+if(mysqli_num_rows($result)>0){
 	while($row = mysqli_fetch_array($result)) {
 		$_SESSION['login']=$row["NOME"];
 		$_SESSION['senha']=$row["SENHA"];
