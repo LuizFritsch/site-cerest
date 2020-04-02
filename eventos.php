42fd2ce8b62e4cb81244ec09ac5ec5d738e8b22b
LuizFritsch <fritsch.guilherm3@gmail.com>
2020-04-02 09:31:55 -0300

    edit eventos
    cards dos eventos agora são responsivos

--- eventos.php
+++ eventos.php
@@ -14,60 +14,66 @@ $con=OpenCon();
 				<h1 id="t" class="text-justify">Eventos</h1>
 				<!--EVENTOS QUE ESTAO ATIVOS-->
 				<h5>Eventos abertos</h5>
-				<?php
-					$sqlEventosAbertos="SELECT * FROM eventos WHERE STATUS=1";
-					$resultEventosAbertos=mysqli_query($con,$sqlEventosAbertos);
-					if(!$resultEventosAbertos) {
-						die('Nao foi possivel coletar os dados dos eventos que estao abertos: ' . mysql_error($con));
-					}else{
-						if(isset($_SESSION['login']) && isset($_SESSION['func'])){
-							while($row = mysqli_fetch_array($resultEventosAbertos)) {
-								echo "<div class='col-xl-3 col-md-6 mb-4'>
-									<div class='card border-0 shadow'>
-										<div class='card-body text-center'>
-											<h5 class='card-title mb-0'>{$row['NOME']}</h5>
-											<div class='card-text text-black-50'><b>{$row['DESCRICAO']}</b></div>
-											<a class='mt-auto btn btn-lg btn-block btn-success'>Inscrever-me</a>
-										</div>
-									</div>
-								  </div>";
+				<div class="container">
+					<div class="row">
+						<?php
+							$sqlEventosAbertos="SELECT * FROM eventos WHERE STATUS=1";
+							$resultEventosAbertos=mysqli_query($con,$sqlEventosAbertos);
+							if(!$resultEventosAbertos) {
+								die('Nao foi possivel coletar os dados dos eventos que estao abertos: ' . mysql_error($con));
+							}else{
+								if(isset($_SESSION['login']) && isset($_SESSION['func'])){
+									while($row = mysqli_fetch_array($resultEventosAbertos)) {
+										echo "<div class='card' style='width: 20rem;'>
+												  <div class='card-body d-flex flex-column'>
+												    <h5 class='my-0 font-weight-normal'>{$row['NOME']}</h5>
+												    <hr>
+												    <p class='card-text'>{$row['DESCRICAO']}</p>
+												    <br>
+												    <a class='mt-auto btn btn-lg btn-block btn-success'>Inscrever-me</a>
+												  </div>
+												</div>";
+									}
+								}else{
+									while($row = mysqli_fetch_array($resultEventosAbertos)) {
+										echo "<div class='card' style='width: 20rem;'>
+												  <div class='card-body d-flex flex-column'>
+												    <h5 class='my-0 font-weight-normal'>{$row['NOME']}</h5>
+												    <hr>
+												    <p class='card-text'>{$row['DESCRICAO']}</p>
+												  </div>
+												</div>";
+									}
+								}
+								
 							}
-						}else{
-							while($row = mysqli_fetch_array($resultEventosAbertos)) {
-								echo "<div class='col-xl-3 col-md-6 mb-4'>
-									<div class='card border-0 shadow'>
-										<div class='card-body text-center'>
-											<h5 class='card-title mb-0'>{$row['NOME']}</h5>
-											<div class='card-text text-black-50'><b>{$row['DESCRICAO']}</b></div>
-										</div>
-									</div>
-								  </div>";
-							}
-						}
-						
-					}
-				?>
+						?>
+					</div>
+				</div>
 				<hr>
 				<!--EVENTOS JA CONCLUIDOS-->
 				<h5>Eventos Ja concluidos</h5>
-				<?php
-					$sqlEventosConcluidos="SELECT * FROM eventos WHERE STATUS=0";
-					$resultEventosConcluidos=mysqli_query($con,$sqlEventosConcluidos);
-					if(!$resultEventosConcluidos) {
-						die('Nao foi possivel coletar os dados dos eventos ja concluidos: ' . mysql_error($con));
-					}else{
-						while($row = mysqli_fetch_array($resultEventosConcluidos)) {
-						echo "<div class='col-xl-3 col-md-6 mb-4'>
-								<div class='card border-0 shadow'>
-									<div class='card-body text-center'>
-										<h5 class='card-title mb-0'>{$row['NOME']}</h5>
-										<div class='card-text text-black-50'><b>{$row['DESCRICAO']}</b></div>
-									</div>
-								</div>
-							  </div>";
-						}
-					}
-				?>
+				<div class="container">
+					<div class="row">
+						<?php
+							$sqlEventosConcluidos="SELECT * FROM eventos WHERE STATUS=0";
+							$resultEventosConcluidos=mysqli_query($con,$sqlEventosConcluidos);
+							if(!$resultEventosConcluidos) {
+								die('Nao foi possivel coletar os dados dos eventos ja concluidos: ' . mysql_error($con));
+							}else{
+								while($row = mysqli_fetch_array($resultEventosConcluidos)) {
+								echo "<div class='card' style='width: 20rem;'>
+												  <div class='card-body d-flex flex-column'>
+												    <h5 class='my-0 font-weight-normal'>{$row['NOME']}</h5>
+												    <hr>
+												    <p class='card-text'>{$row['DESCRICAO']}</p>
+												  </div>
+												</div>";
+								}
+							}
+						?>
+					</div>
+				</div>
 				<br>
 			</div>
 		</main>
