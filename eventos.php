9f113ef5958c4afb9029c42680c2fd9d67231739
LuizFritsch <fritsch.guilherm3@gmail.com>
2020-04-02 08:59:17 -0300

    edit eventos
    sql:
         -criei tabela inscritos_eventos que tem a fk id do evento e a fk id do usuario que se inscreveu
    eventos:
         -apresenta os eventos abertos e os que ja foram fechados

--- eventos.php
+++ eventos.php
@@ -1,4 +1,8 @@
 <?php include 'HEADER.php'; ?>
+<?php
+include './database/db_connection.php';
+$con=OpenCon();
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,11 +13,48 @@
 			<div class="content text-break">
 				<h1 id="t" class="text-justify">Eventos</h1>
 				<!--EVENTOS QUE ESTAO ATIVOS-->
-				<!--CARD-->
-				
+				<h5>Eventos abertos</h5>
+				<?php
+					$sqlEventosAbertos="SELECT * FROM eventos WHERE STATUS=1";
+
+					$resultEventosAbertos=mysqli_query($con,$sqlEventosAbertos);
+					if(!$resultEventosAbertos) {
+						die('Nao foi possivel coletar os dados dos eventos que estao abertos: ' . mysql_error($con));
+					}else{
+						while($row = mysqli_fetch_array($resultEventosAbertos)) {
+						echo "<div class='col-xl-3 col-md-6 mb-4'>
+								<div class='card border-0 shadow'>
+									<div class='card-body text-center'>
+										<h5 class='card-title mb-0'>{$row['NOME']}</h5>
+										<div class='card-text text-black-50'><b>{$row['DESCRICAO']}</b></div>
+									</div>
+								</div>
+							  </div>";
+						}
+					}
+				?>
 				<hr>
 				<!--EVENTOS JA CONCLUIDOS-->
-
+				<h5>Eventos Ja concluidos</h5>
+				<?php
+					$sqlEventosConcluidos="SELECT * FROM eventos WHERE STATUS=0";
+					$resultEventosConcluidos=mysqli_query($con,$sqlEventosConcluidos);
+					if(!$resultEventosConcluidos) {
+						die('Nao foi possivel coletar os dados dos eventos ja concluidos: ' . mysql_error($con));
+					}else{
+						while($row = mysqli_fetch_array($resultEventosConcluidos)) {
+						echo "<div class='col-xl-3 col-md-6 mb-4'>
+								<div class='card border-0 shadow'>
+									<div class='card-body text-center'>
+										<h5 class='card-title mb-0'>{$row['NOME']}</h5>
+										<div class='card-text text-black-50'><b>{$row['DESCRICAO']}</b></div>
+									</div>
+								</div>
+							  </div>";
+						}
+					}
+				?>
+				<br>
 			</div>
 		</main>
 		<?php include 'footer.html'; ?>
