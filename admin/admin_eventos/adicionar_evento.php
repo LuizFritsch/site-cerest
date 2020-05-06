9fe57f18dff3957c768b22b42107e8bfbebbf527
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2020-05-06 11:50:06 -0300

    edit eventos
    -corrigi titulo da pagina de add evento

--- admin/admin_eventos/adicionar_evento.php
+++ admin/admin_eventos/adicionar_evento.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Criar Eventos</title>
+		<title>Adicionar Evento</title>
 
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
